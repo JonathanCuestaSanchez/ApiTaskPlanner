f0a0e0290b234e2039576c1ca8b218c2745b6d5e
JonathanCuestaSanchez <jonathancuesta97@gmail.com>
2019-11-19 22:30:16 -0500

    secure api

--- src/main/java/com/example/ApiTask/controller/UserController.java
+++ src/main/java/com/example/ApiTask/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.ApiTask.controller;
 
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
 
 import com.example.ApiTask.model.User;
 import com.example.ApiTask.persistence.UserService;
@@ -17,7 +20,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
  * UserController
@@ -76,6 +83,61 @@ public class UserController {
         } catch (Exception ex) {
 
             return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping( value = "/login", method = RequestMethod.POST )
+    public Token login( @RequestBody User login )
+        throws ServletException
+    {
+        String jwtToken = "";
+
+        if ( login.getEmail() == null || login.getPassword() == null )
+        {
+            throw new ServletException( "Please fill in username and password" );
+        }
+        String username = login.getEmail();
+        String password = login.getPassword();       
+        User user = userService.getUserByEmail( username );
+        if ( user == null )
+        {
+            throw new ServletException( "User username not found." );
+        }
+
+        String pwd = user.getPassword();
+
+        if ( !password.equals( pwd ) )
+        {
+            throw new ServletException( "Invalid login. Please check your name and password." );
+        }
+        //
+        jwtToken = Jwts.builder().setSubject( username ).claim( "roles", "user" ).setIssuedAt( new Date() ).signWith(
+            
+            SignatureAlgorithm.HS256, "secretkey" ).compact();
+
+        return new Token( jwtToken );
+    }
+
+    public class Token
+    {
+
+        String accessToken;
+
+
+        public Token( String accessToken )
+        {
+            this.accessToken = accessToken;
+        }
+
+
+        public String getAccessToken()
+        {
+            return accessToken;
+        }
+
+        public void setAccessToken( String access_token )
+        {
+            this.accessToken = access_token;
         }
     }
 
